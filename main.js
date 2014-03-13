145ff614170837492a595b15b4d565e3c569f6fd
Becky Bell <rlbell@alum.bu.edu>
2014-03-13 00:12:53 -0400

    added comments to code + explanation.txt file

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
+//load the script when the brower window opens
 window.onload = function() {
 
+//tells d3 to use the csv file named data
 d3.csv("data.csv", function(csv){
 
-var svg = d3.select("body")
-  .data(csv)
-.enter().append("circle")
-  .attr("r", 40)
-  .attr("cx", function(d) {return d.Women})
-  .attr("cy", function(d) {return d.Men})
-.selectAll("circle")
-  .style("fill", 'steelblue')
-  .style("stroke", 'orange')
+//defines two different colors
+var color_women = 'red';
+var color_men = 'pink';
 
-  .append("title")
-  .text(function(d) {return d.disease;})
+
+var svg = d3.select("body") //defines svg, tells d3 to select the body element
+  .data(csv) //tells d3 that the data is in the csv file
+.enter().append("circle") //tells d3 to iterate over the data and add a circle for each element in the array
+  .attr("r", 40) //defines the radius of each circle as 40
+  .attr("cx", 50) //defines the x coordinates for the center of the circle
+  .attr("cy", 50) //defines the y coordinates for the center of the circle
+  .style("stroke-width", function(d) { return d.Women })//tells d3 to give all of the circles a stoke width the same size as the value given in the women column
+.selectAll("circle") //tells d3 to select all of the circles
+  .style("stroke", color_women) //tells d3 to give all of the circles a red stroke
+  .style("fill", color_men) //tells d3 to give all of the circles a pink fill
+  
+  svg.append("title") //tells d3 to select the title element
+  .text(function(d) {return d.disease;});//tells d3 to add the text from the disease column in the csv file
 
 })
 
