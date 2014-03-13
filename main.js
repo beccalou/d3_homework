8a93e0ce64ed233684fc03cf2bfeaffa07fa734a
Becky Bell <rlbell@alum.bu.edu>
2014-03-12 23:12:58 -0400

    connected data in csv file to circles-- 1 circle for each disease

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 window.onload = function() {
 
-d3.csv("data.csv", function(healthData){
+d3.csv("data.csv", function(csv){
 
-svg.selectAll("circle").data(csv).enter()
-  .append("circle")
-  .attr("cx"), function(d) {return x(0);})
-  .attr("cy"), function(d) {return x(0);})
-  .attr("r"), function(d) {return x(0);})
+var svg = d3.select("body")
+  .data(csv)
+.enter().append("circle")
+  .attr("r", 40)
+  .attr("cx", function(d) {return d.Women})
+  .attr("cy", function(d) {return d.Men})
+.selectAll("circle")
+  .style("fill", 'steelblue')
+  .style("stroke", 'orange')
 
+  .append("title")
+  .text(function(d) {return d.disease;})
 
 })
 
